ddf40e3a81abed0c792c29f168f81063d9aba600
valeno12 <99143152+valeno12@users.noreply.github.com>
2022-02-07 14:54:45 -0300

    Add files via upload

--- sin_titulo.cpp
+++ sin_titulo.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 string catedras[100][3];
 int cont=0, cont2=0;
-void archivos();
+void archivos(), libres();
+string buscar_catedra(string code);
 int busqueda(string dni, string cod);
 struct alu{
 	int libres;
@@ -25,11 +26,11 @@ doc docentes[100];
 alu alumnos[100];
 int main(int argc, char *argv[]) {
 	archivos();
-	int k=0, y=0;
+	int k=0, y=0, cont_a=0;
 	string dni, code;
 	cout << "Ingrese DNI: "; cin >> dni; cout << endl;
-	//while (dni != aux);
-	{
+	//while (dni != "0");
+	//{
 		cout << "Ingrese codigo de catedra: "; cin >> code; cout << endl;
 		y=busqueda(dni, code);
 		if (docentes[y].cargo == "JTP" || docentes[y].cargo == "AUXILIAR")
@@ -41,10 +42,30 @@ int main(int argc, char *argv[]) {
 			cout << "Ingrese cantidad de alumnos libres: "; cin >> alumnos[k].libres;
 			cout << "Ingrese cantidad de alumnos regulares: "; cin >> alumnos[k].regulares;
 			cout << "Ingrese cantidad de alumnos promocionados: "; cin >> alumnos[k].prom;
+			alumnos[k].catedra = buscar_catedra(code);
+			cont_a++;
+			k++;
 		}
-		cout << "Ingrese DNI: "; cin >> dni; cout << endl;
-		cout << "Ingrese codigo de catedra; "; cin >> code; cout << endl;
-	};
+		/*cout << "Ingrese DNI: "; cin >> dni; cout << endl;
+		cout << "Ingrese codigo de catedra; "; cin >> code; cout << endl;*/
+	//};
+	ofstream archivo3;
+	archivo3.open("fin_cursada.txt");
+	int auxa=0;
+	if (archivo3.fail())
+	{
+		cout << "ERROR";
+	}
+	else
+	{
+		archivo3 << "Nombre ded catedra" << "                      " << "Libres" << "   " << "No libres" << endl;
+		for (int i=0; i<cont_a; i++)
+		{
+			auxa = alumnos[i].prom + alumnos[i].regulares;
+			archivo3 << alumnos[i].catedra << "                 " << alumnos[i].libres << "     " << auxa << endl;
+		}
+	}
+	libres();
 	return 0;
 }
 
@@ -105,4 +126,31 @@ int busqueda(string dni, string cod)
 		};
 	}
 	return pos;
+}
+string buscar_catedra(string code)
+{
+	string y;
+	for (int j=0; j<cont; j++)
+	{
+		if (code == catedras[j][0])
+		{
+			y=catedras[j][2];
+			j=cont;
+		}
+	}
+	return y;
+}
+void libres()
+{
+	string y= "2", x;
+	cout << "NOMBRE CATEDRA" << "            " << "AÑO CURSADA" << endl;
+	for (int i=0; i<cont; i++)
+	{
+		//y = catedras[i][0].substr(3, 1);
+		x = catedras[i][0].substr(6, 1);
+		if (x == "S")
+		{
+			cout << catedras[i][2] << "      " << y << endl;
+		}
+	}
 }
